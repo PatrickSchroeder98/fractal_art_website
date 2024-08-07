75ddba489e6620dbdcd2cf1298fef1cb04d385a7
Patrick Schroder <patrickschroder479@gmail.com>
2024-08-07 14:19:39 +0200

    Added main page, images and translations
    Designed and implemented main page, added images used on the page, added header and footer, added translations file.

---
+++ static/js/translations.js
@@ -0,0 +1,25 @@
+const translations = {
+    en: {
+        "title": "Fractal Art Website",
+        "logo": "Fractal Art Website",
+        "nav-home": "Home",
+        "nav-about": "About me",
+        "hero-title": "Welcome to Fractal Art Website",
+        "hero-text": "I'm Patrick Schröder, a software engineer with a master's degree in computer science. On this website, I'm presenting my art made with fractals, blending mathematics and creativity. Explore my works and get in touch!",
+        "ins-title": "Inspiration",
+        "sfd-title": "Spring Flowers Draft",
+        "jsd-title": "Julia Set Draft",
+        "msd-title": "Mandelbrot Set Draft",
+        "view-more": "View more",
+        "copyright": "\xa9 2024, Patrick Schröder, Fractal Art Website v1.0.0. All rights reserved. All logos are trademarks of their respective owners and are used here for informational purposes only.",
+
+    },
+
+    pl: {
+
+    },
+
+    de: {
+
+    }
+}
