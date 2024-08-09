37c1b7ab26ff53e4a40d712e9f3cf3f3a078fec6
Patrick Schroder <patrickschroder479@gmail.com>
2024-08-09 13:06:01 +0200

    Added 3 pages for paintings, replaced background
    Added page for inspiration painting, updated the translations file, added pages for Spring Flowers Draft and Mandelbrot Set Draft. Replaced background image of website.

--- static/js/translations.js
+++ static/js/translations.js
@@ -6,11 +6,30 @@ const translations = {
         "nav-about": "About me",
         "hero-title": "Welcome to Fractal Art Website",
         "hero-text": "On this website, I'm presenting my art made with fractals, blending mathematics and creativity. Explore my works and get in touch!",
-        "ins-title": "Inspiration",
+        "ins-title": `"Inspiration"`,
         "sfd-title": "Spring Flowers Draft",
         "jsd-title": "Julia Set Draft",
         "msd-title": "Mandelbrot Set Draft",
         "view-more": "View more",
+
+        "about-title": "About me",
+        "about-text": `I'm Patrick Schröder, a software engineer with a master's degree in computer science. My passion for fractals started when I got an assignment at the University of Łódź to generate the Mandelbrot set. Since then, I have been creating designs, exploring the set by zooming in on various coordinates. <br><br><br> 
+
+                    I also started to develop Julia set drafts as well as other fractals. My recent painting 'Anxiety' uses the shape of the Burning Ship fractal. Of course, there is more to come in the future. <br><br><br> 
+                    
+                    The Julia set is worth mentioning here because I'm working with a version that includes a self-discovered modification, making the background shapes much more visible. <br><br><br> 
+                    
+                    I also experiment with merging fractals with pictures. One particular achievement in this field is the draft of 'Spring Flowers.' You can see this painting printed and framed. <br><br><br>  
+                    
+                    Now, I'm still creating various pieces of art, merging the sets and coloring them with RGB or HSV functions. The best paintings are printed and framed. The details are presented on this website. <br><br><br> 
+                    
+                    To see more of my works, visit my`,
+        "ins-artist": "<strong>Artist:</strong> Patrick Schröder",
+        "ins-year": "<strong>Year:</strong> 2024</p>",
+        "ins-medium": "<strong>Medium:</strong> Poster paper 200; printer: Eco Solvent Mutoh",
+        "ins-size": "<strong>Size:</strong> 124,5 cm x 89 cm",
+        "ins-description": "This painting represents an idea forming in the mind. The viewer can see the clarified idea in the center, as well as a shadow in the background representing the idea that has not fully formed. Both the idea and the shadow share the same shape, symbolizing the connection between these two elements. Some parts of the idea are visible behind a fog, representing the need for learning and exploration while being inspired.",
+
         "copyright": "\xa9 2024, Patrick Schröder, Fractal Art Website v1.0.0. All rights reserved. All logos are trademarks of their respective owners and are used here for informational purposes only.",
 
     },
