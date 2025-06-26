39e3df7a89b564b5bf9d47988f71546c29a0c4d3
Patrick Schröder <patrickschroder479@gmail.com>
2025-06-26 21:14:48 +0200

    Update 1.1.3 - second update to Selenium tests
    Updated Selenium tests, reformated code with Black library, small fixes.

--- static/js/translations.js
+++ static/js/translations.js
@@ -54,7 +54,7 @@ const translations = {
         "jsd-size": "Size: Various resolutions of images",
         "jsd-description": "The Julia set is the second fractal that I'm exploring. Similar to the Mandelbrot set, I'm using various zoom levels and coloring functions. I also include a self-discovered modification in the equation that makes the lines in the background much more visible.",
 
-        "copyright": "\xa9 2024, Patrick Schröder, Fractal Art Website v1.1.0. All rights reserved. All logos are trademarks of their respective owners and are used here for informational purposes only.",
+        "copyright": "\xa9 2024, Patrick Schröder, Fractal Art Website v1.1.3. All rights reserved. All logos are trademarks of their respective owners and are used here for informational purposes only.",
     },
 
     pl: {
@@ -112,7 +112,7 @@ const translations = {
         "jsd-size": "Rozmiar: Różne rozdzielczości obrazów",
         "jsd-description": "Zbiór Julii to drugi fraktal, który eksploruję. Podobnie jak zbiór Mandelbrota, używam różnych poziomów powiększenia i funkcji kolorowania. Zawiera również samodzielnie odkrytą modyfikację w równaniu, która sprawia, że linie w tle stają się znacznie bardziej widoczne.",
 
-        "copyright": "\xa9 2024, Patrick Schröder, Strona o Fraktalnej Sztuce v1.1.0. Wszelkie prawa zastrzeżone. Wszystkie loga są znakami towarowymi ich właścicieli i są używane tutaj wyłącznie w celach informacyjnych.",
+        "copyright": "\xa9 2024, Patrick Schröder, Strona o Fraktalnej Sztuce v1.1.3. Wszelkie prawa zastrzeżone. Wszystkie loga są znakami towarowymi ich właścicieli i są używane tutaj wyłącznie w celach informacyjnych.",
     },
 
     de: {
@@ -170,6 +170,6 @@ const translations = {
         "jsd-size": "Größe: Verschiedene Bildauflösungen",
         "jsd-description": "Die Julia-Menge ist das zweite Fraktal, das ich erkunde. Ähnlich wie bei der Mandelbrot-Menge verwende ich verschiedene Zoomstufen und Farbgebungsfunktionen. Ich nehme auch eine selbst entdeckte Modifikation in die Gleichung auf, die die Linien im Hintergrund viel sichtbarer macht.",
 
-        "copyright": "\xa9 2024, Patrick Schröder, Fraktal-Kunst-Website v1.1.0. Alle Rechte vorbehalten. Alle Logos sind Marken ihrer jeweiligen Besitzer und werden hier nur zu Informationszwecken verwendet.",
+        "copyright": "\xa9 2024, Patrick Schröder, Fraktal-Kunst-Website v1.1.3. Alle Rechte vorbehalten. Alle Logos sind Marken ihrer jeweiligen Besitzer und werden hier nur zu Informationszwecken verwendet.",
     }
 }
--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+
 class FractalArtWebsiteTests(unittest.TestCase):
     """Class for Fractal Art Website Tests."""
 
@@ -10,7 +11,8 @@ class FractalArtWebsiteTests(unittest.TestCase):
     def setUpClass(cls):
         """Method that prepares the driver for testing"""
         cls.driver = webdriver.Chrome()
-        cls.base_url = "https://patrickschroeder98.github.io/fractal_art_website/index.html"
+        # cls.base_url = "https://patrickschroeder98.github.io/fractal_art_website/index.html"
+        cls.base_url = "http://127.0.0.1:5500/index.html"
         cls.driver.get(cls.base_url)
 
     def setUp(self):
@@ -25,31 +27,41 @@ class FractalArtWebsiteTests(unittest.TestCase):
 
     def test_religiosity(self):
         """Method that checks if the religiosity title is displayed correctly."""
-        self.driver.find_element(By.CSS_SELECTOR, "h2[data-i18n='rel-title'] + a").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "h2[data-i18n='rel-title'] + a"
+        ).click()
         title = self.driver.title
         self.assertEqual("Religiosity - Fractal Art", title)
 
     def test_inspiration(self):
         """Method that checks if the inspiration title is displayed correctly."""
-        self.driver.find_element(By.CSS_SELECTOR, "h2[data-i18n='ins-title'] + a").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "h2[data-i18n='ins-title'] + a"
+        ).click()
         title = self.driver.title
         self.assertEqual("Inspiration - Fractal Art", title)
 
     def test_spring_flowers(self):
         """Method that checks if the spring flowers title is displayed correctly."""
-        self.driver.find_element(By.CSS_SELECTOR, "h2[data-i18n='sfd-title'] + a").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "h2[data-i18n='sfd-title'] + a"
+        ).click()
         title = self.driver.title
         self.assertEqual("Spring Flowers Draft - Fractal Art", title)
 
     def test_julia_set(self):
         """Method that checks if the julia set drafts title is displayed correctly."""
-        self.driver.find_element(By.CSS_SELECTOR, "h2[data-i18n='jsd-title'] + a").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "h2[data-i18n='jsd-title'] + a"
+        ).click()
         title = self.driver.title
         self.assertEqual("Julia Set Drafts - Fractal Art", title)
 
     def test_mandelbrot_set(self):
         """Method that checks if the mandelbrot set drafts title is displayed correctly."""
-        self.driver.find_element(By.CSS_SELECTOR, "h2[data-i18n='msd-title'] + a").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "h2[data-i18n='msd-title'] + a"
+        ).click()
         title = self.driver.title
         self.assertEqual("Mandelbrot Set Drafts - Fractal Art", title)
 
@@ -87,123 +99,242 @@ class FractalArtWebsiteTests(unittest.TestCase):
 
     def test_img_mainpage_rel(self):
         """Method that checks if image on the main page has correct attributes."""
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/religiosity.jpg']")
+        img = self.driver.find_element(
+            By.CSS_SELECTOR, "img[src='./assets/img/religiosity.jpg']"
+        )
         self.assertIsNotNone(img)
         self.assertEqual(img.get_attribute("alt"), "Religiosity")
 
     def test_img_mainpage_ins(self):
         """Method that checks if image on the main page has correct attributes."""
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/inspiration.jpg']")
+        img = self.driver.find_element(
+            By.CSS_SELECTOR, "img[src='./assets/img/inspiration.jpg']"
+        )
         self.assertIsNotNone(img)
         self.assertEqual(img.get_attribute("alt"), "Inspiration")
 
     def test_img_mainpage_sfd(self):
         """Method that checks if image on the main page has correct attributes."""
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/flowers.jpg']")
+        img = self.driver.find_element(
+            By.CSS_SELECTOR, "img[src='./assets/img/flowers.jpg']"
+        )
         self.assertIsNotNone(img)
         self.assertEqual(img.get_attribute("alt"), "Spring Flowers Draft")
 
     def test_img_mainpage_jsd(self):
         """Method that checks if image on the main page has correct attributes."""
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/set9.png']")
+        img = self.driver.find_element(
+            By.CSS_SELECTOR, "img[src='./assets/img/set9.png']"
+        )
         self.assertIsNotNone(img)
         self.assertEqual(img.get_attribute("alt"), "Julia Set")
 
     def test_img_mainpage_msd(self):
         """Method that checks if image on the main page has correct attributes."""
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/set3.png']")
+        img = self.driver.find_element(
+            By.CSS_SELECTOR, "img[src='./assets/img/set3.png']"
+        )
         self.assertIsNotNone(img)
         self.assertEqual(img.get_attribute("alt"), "Mandelbrot Set")
 
     def test_img_about(self):
         """Method that checks if image on the 'about' page has correct attributes."""
         self.driver.find_element(By.CSS_SELECTOR, "a[data-i18n='nav-about']").click()
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/sesja3153.jpg']")
+        img = self.driver.find_element(
+            By.CSS_SELECTOR, "img[src='./assets/img/sesja3153.jpg']"
+        )
         self.assertIsNotNone(img)
         self.assertEqual(img.get_attribute("alt"), "Artist Photo")
 
-
     def test_img_religiosity(self):
         """Method that checks if image on the religiosity page has correct attributes."""
-        self.driver.find_element(By.CSS_SELECTOR, "h2[data-i18n='rel-title'] + a").click()
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/religiosity.jpg']")
+        self.driver.find_element(
+            By.CSS_SELECTOR, "h2[data-i18n='rel-title'] + a"
+        ).click()
+        img = self.driver.find_element(
+            By.CSS_SELECTOR, "img[src='./assets/img/religiosity.jpg']"
+        )
         self.assertIsNotNone(img)
         self.assertEqual(img.get_attribute("alt"), "Religiosity Painting")
 
     def test_img_inspiration(self):
         """Method that checks if image on the inspiration page has correct attributes."""
-        self.driver.find_element(By.CSS_SELECTOR, "h2[data-i18n='ins-title'] + a").click()
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/inspiration.jpg']")
+        self.driver.find_element(
+            By.CSS_SELECTOR, "h2[data-i18n='ins-title'] + a"
+        ).click()
+        img = self.driver.find_element(
+            By.CSS_SELECTOR, "img[src='./assets/img/inspiration.jpg']"
+        )
         self.assertIsNotNone(img)
         self.assertEqual(img.get_attribute("alt"), "Inspiration Painting")
 
     def test_img_flowers(self):
         """Method that checks if image on the spring flowers page has correct attributes."""
-        self.driver.find_element(By.CSS_SELECTOR, "h2[data-i18n='sfd-title'] + a").click()
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/flowers.jpg']")
+        self.driver.find_element(
+            By.CSS_SELECTOR, "h2[data-i18n='sfd-title'] + a"
+        ).click()
+        img = self.driver.find_element(
+            By.CSS_SELECTOR, "img[src='./assets/img/flowers.jpg']"
+        )
         self.assertIsNotNone(img)
         self.assertEqual(img.get_attribute("alt"), "Spring Flowers Draft")
 
     def test_img_jsd(self):
         """Method that checks if images on the julia set page have correct attributes."""
-        self.driver.find_element(By.CSS_SELECTOR, "h2[data-i18n='jsd-title'] + a").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "h2[data-i18n='jsd-title'] + a"
+        ).click()
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/set9.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Draft - White On Black")
+        def run(val, txt):
+            img = self.driver.find_element(By.CSS_SELECTOR, val)
+            self.assertIsNotNone(img)
+            self.assertEqual(img.get_attribute("alt"), txt)
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/set9a.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Draft - Red On White")
+        values = [
+            "img[src='./assets/img/set9.png']",
+            "img[src='./assets/img/set9a.png']",
+            "img[src='./assets/img/set2.png']",
+            "img[src='./assets/img/set888.png']",
+        ]
+        texts = [
+            "Draft - White On Black",
+            "Draft - Red On White",
+            "Close-Up - Black On White",
+            "Close-Up - Red On Yellow",
+        ]
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/set2.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Close-Up - Black On White")
+        self.assertEqual(len(values), len(texts))
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/set888.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Close-Up - Red On Yellow")
+        for i in range(len(values)):
+            run(values[i], texts[i])
 
     def test_img_msd(self):
         """Method that checks if images on the mandelbrot set page have correct attributes."""
-        self.driver.find_element(By.CSS_SELECTOR, "h2[data-i18n='msd-title'] + a").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "h2[data-i18n='msd-title'] + a"
+        ).click()
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/set3.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Draft - Black On White")
+        def run(val, txt):
+            img = self.driver.find_element(By.CSS_SELECTOR, val)
+            self.assertIsNotNone(img)
+            self.assertEqual(img.get_attribute("alt"), txt)
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/2407_3.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Close-Up - White On Black")
+        values = [
+            "img[src='./assets/img/set3.png']",
+            "img[src='./assets/img/2407_3.png']",
+            "img[src='./assets/img/hsv9a.png']",
+            "img[src='./assets/img/hsv_rotated_spiral_zoom2.png']",
+            "img[src='./assets/img/hsv_rotated_spiral_zoom3.png']",
+            "img[src='./assets/img/hsv4.png']",
+            "img[src='./assets/img/hsv_rotated_spiral_zoom4.png']",
+            "img[src='./assets/img/hsv_rotated_spiral_zoom5.png']",
+        ]
+        texts = [
+            "Draft - Black On White",
+            "Close-Up - White On Black",
+            "Close-Up - Black on Blue-Red",
+            "Close-Up - Black on Blue-Purple",
+            "Close-Up - Black on Green Spectrum",
+            "Close-Up - Red on Green-Blue Spectrum",
+            "Close-Up - Black on Blue",
+            "Close-Up - Black on Pink-Purple Spectrum",
+        ]
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/hsv9a.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Close-Up - Black on Blue-Red")
+        self.assertEqual(len(values), len(texts))
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/hsv_rotated_spiral_zoom2.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Close-Up - Black on Blue-Purple")
+        for i in range(len(values)):
+            run(values[i], texts[i])
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/hsv_rotated_spiral_zoom3.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Close-Up - Black on Green Spectrum")
+    def test_copyright(self):
+        """Method that checks if the copyright text in english is displayed correctly."""
+        cpright = "All rights reserved. All logos are trademarks of their respective owners and are used here for informational purposes only."
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/hsv4.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Close-Up - Red on Green-Blue Spectrum")
+        def check():
+            txt = self.driver.find_element(By.CSS_SELECTOR, "p[data-i18n='copyright']")
+            self.assertIsNotNone(txt)
+            self.assertTrue(cpright in txt.text)
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/hsv_rotated_spiral_zoom4.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Close-Up - Black on Blue")
+        def run(val):
+            self.setUp()
+            self.driver.find_element(By.CSS_SELECTOR, val).click()
+            check()
 
-        img = self.driver.find_element(By.CSS_SELECTOR, "img[src='./assets/img/hsv_rotated_spiral_zoom5.png']")
-        self.assertIsNotNone(img)
-        self.assertEqual(img.get_attribute("alt"), "Close-Up - Black on Pink-Purple Spectrum")
+        values = [
+            "a[data-i18n='nav-about']",
+            "h2[data-i18n='rel-title'] + a",
+            "h2[data-i18n='ins-title'] + a",
+            "h2[data-i18n='sfd-title'] + a",
+            "h2[data-i18n='jsd-title'] + a",
+            "h2[data-i18n='msd-title'] + a",
+        ]
+
+        check()
+        for val in values:
+            run(val)
+
+    def test_copyright_pl(self):
+        """Method that checks if the copyright text in polish is displayed correctly."""
+        cpright = "Wszelkie prawa zastrzeżone. Wszystkie loga są znakami towarowymi ich właścicieli i są używane tutaj wyłącznie w celach informacyjnych."
+
+        def check():
+            txt = self.driver.find_element(By.CSS_SELECTOR, "p[data-i18n='copyright']")
+            self.assertIsNotNone(txt)
+            self.assertTrue(cpright in txt.text)
+
+        def run(val):
+            self.setUp()
+            self.driver.find_element(By.CSS_SELECTOR, "a[data-lang='pl']").click()
+            self.driver.find_element(By.CSS_SELECTOR, val).click()
+            check()
+
+        values = [
+            "a[data-i18n='nav-about']",
+            "h2[data-i18n='rel-title'] + a",
+            "h2[data-i18n='ins-title'] + a",
+            "h2[data-i18n='sfd-title'] + a",
+            "h2[data-i18n='jsd-title'] + a",
+            "h2[data-i18n='msd-title'] + a",
+        ]
+
+        self.driver.find_element(By.CSS_SELECTOR, "a[data-lang='pl']").click()
+        check()
+        for val in values:
+            run(val)
+
+    def test_copyright_de(self):
+        """Method that checks if the copyright text in german is displayed correctly."""
+        cpright = "Alle Rechte vorbehalten. Alle Logos sind Marken ihrer jeweiligen Besitzer und werden hier nur zu Informationszwecken verwendet."
+
+        def check():
+            txt = self.driver.find_element(By.CSS_SELECTOR, "p[data-i18n='copyright']")
+            self.assertIsNotNone(txt)
+            self.assertTrue(cpright in txt.text)
+
+        def run(val):
+            self.setUp()
+            self.driver.find_element(By.CSS_SELECTOR, "a[data-lang='de']").click()
+            self.driver.find_element(By.CSS_SELECTOR, val).click()
+            check()
+
+        values = [
+            "a[data-i18n='nav-about']",
+            "h2[data-i18n='rel-title'] + a",
+            "h2[data-i18n='ins-title'] + a",
+            "h2[data-i18n='sfd-title'] + a",
+            "h2[data-i18n='jsd-title'] + a",
+            "h2[data-i18n='msd-title'] + a",
+        ]
+
+        self.driver.find_element(By.CSS_SELECTOR, "a[data-lang='de']").click()
+        check()
+        for val in values:
+            run(val)
 
     @classmethod
     def tearDownClass(cls):
         """Method that runs after testing."""
         cls.driver.quit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
